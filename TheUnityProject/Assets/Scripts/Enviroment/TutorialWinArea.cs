7fcf68971b234bc464201977ae1016facfbe82ae
Deltager <teknisk+GDC2023-GR3@game.unf.dk>
2023-07-19 23:13:44 +0200

    Update TutorialWinArea.cs

--- TheUnityProject/Assets/Scripts/Enviroment/TutorialWinArea.cs
+++ TheUnityProject/Assets/Scripts/Enviroment/TutorialWinArea.cs
@@ -7,11 +7,30 @@ public class TutorialWinArea : MonoBehaviour
 {
     private const string PLAYER_TAG = "Player";
 
+    private int openHash = Animator.StringToHash("Open");
+    
+    [SerializeField] private float waitTime;
+    [SerializeField] private Animator animator;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(PLAYER_TAG))
         {
-            SceneHandler.Instance.LoadScene(SceneHandler.Instance.levelOneSceneIndex, MusicPlayer.Instance.gameMusic, SceneLoadType.Load);
+            SceneHandler.Instance.LoadScene(SceneHandler.Instance.mainMenuSceneIndex, MusicPlayer.Instance.gameMusic, SceneLoadType.Load);
+            
+            if (animator != null)
+            {
+                animator.SetTrigger(openHash);
+            }
+
+            StartCoroutine(LoadScene());
         }
+    }
+    
+    private IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(waitTime);
+        
+        SceneHandler.Instance.LoadScene(SceneHandler.Instance.mainMenuSceneIndex, MusicPlayer.Instance.menuMusic, SceneLoadType.Load);
     }
 }
